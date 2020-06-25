f56bae743c0a5d52f0201ffae02ef974543125cd
LOCNg0114 <58358673+LOCNg0114@users.noreply.github.com>
2020-06-25 21:10:57 +0700

    Update ArmStrongNum.cpp

--- ArmStrongNum.cpp
+++ ArmStrongNum.cpp
@@ -4,12 +4,22 @@
 using namespace std;
 
 bool ArmstrongNum(int n){
-    #TODO
+    int sum = 0;
+    int temp = n;
+    int num_digit = (int)log10(n) + 1; // total numbers of all digit
+    int digit;
+    while (temp > 0){
+        digit = temp % 10; // take the last digit out
+        sum += pow(digit, num_digit);
+        temp /= 10;
+    }
+    if (sum == n) return true;
+    else return false;
 }
 
 int main(int arg, char** argv){
     ifstream ifs;
-	ifs.open(argv[1]);
+	ifs.open("testcase.txt");
 	int N;
 	try
 	{
